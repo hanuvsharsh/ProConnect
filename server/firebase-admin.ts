0a507c93c256ddf71733a02b57bd83923c49e8cb
Harshvardhan Singh <harshvardhansingh@Harshvardhans-MacBook-Air.local>
2025-08-04 14:02:26 +0530

    Fix: relative import for firebase-admin

--- server/firebase-admin.ts
+++ server/firebase-admin.ts
@@ -1,11 +1,14 @@
-// server/lib/firebase-admin.ts
 import admin from "firebase-admin";
-import serviceAccount from "./config/serviceAccountKey.json";
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  });
-}
+const serviceAccountJSON = Buffer.from(
+  process.env.FIREBASE_KEY_BASE64!,
+  "base64"
+).toString("utf-8");
 
+const serviceAccount = JSON.parse(serviceAccountJSON);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 export { admin };
+
